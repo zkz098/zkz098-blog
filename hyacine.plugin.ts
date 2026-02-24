9560080cda9d61d839d7a8a7542c9bce06967a3b
zkz098 <zkz20081204@126.com>
2026-02-24 11:45:15 +0800

    chore: 生态系统适配

--- hyacine.plugin.ts
+++ hyacine.plugin.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "@hyacine/core/config";
 
+import SiteUpTime from "@hyacine/site-uptime"
+import MouseFirework from "@hyacine/mouse-firework"
+
 export default defineConfig({
   injectPoints: {
     // 页面 head 部分，用于注入元标签、CSS 等
@@ -19,5 +22,19 @@ export default defineConfig({
     "post-footer": ".post footer",
   },
 
-  plugins: [],
+  plugins: [
+    SiteUpTime({
+      siteCreatedAt: "2022-08-01T00:00:00Z",
+    }),
+    MouseFirework({
+      colors: [
+        "rgba(255,182,185,.9)",
+        "rgba(250,227,217,.9)",
+        "rgba(187,222,214,.9)",
+        "rgba(138,198,209,.9)"
+      ],
+      count: 30,
+      radius: 32
+    })
+  ],
 });
